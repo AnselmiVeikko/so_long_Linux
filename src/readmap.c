46b6f714b5107c78975865b8fa3ede14c2a4b206
Anselmi Henttonen <ahentton@c3r2p3.hive.fi>
2024-08-21 11:19:15 +0300

    created the flood-fill function. Now little pause while I configure my nvim settings.. norminette doesn't like my settings...

--- src/readmap.c
+++ src/readmap.c
@@ -6,49 +6,60 @@
 /*   By: ahentton <ahentton@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 16:19:49 by ahentton          #+#    #+#             */
-/*   Updated: 2024/08/16 16:19:50 by ahentton         ###   ########.fr       */
+/*   Updated: 2024/08/21 11:18:03 by ahentton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-void read_map(char *argv, t_game *game)
+void	read_map(char *argv, t_game *game)
 {
-  int   fd_map;
-  char  *temp;
-  char  *big_str;
-  char  *str;
+	int		map;
+	char	*line;
+	char	*map_buffer;
 
-  fd_map = open(argv, O_RDONLY);
-  big_str = ft_strdup("");
-  if (fd_map == -1)
-    ft_printf("File error or empty");
-  str = get_next_line(fd_map);
-  if (!str)
-    ft_printf("Empty file");
- while (str)
-  {
-    temp = ft_strjoin(big_str, str);
-    free (big_str);
-    big_str = temp;
-    free (str);
-    str = get_next_line(fd_map);
-  }
-  free (str);
-  free (temp);
-  close(fd_map);
-  game->map = ft_split(big_str, '\n');
-  game->mapcopy = ft_split(big_str, '\n');
+	map = open(argv, O_RDONLY);
+	map_buffer = ft_strdup("");
+	if (map == -1)
+		ft_printf("[ERROR]: The file is invalid or empty");
+	line = get_next_line(map);
+	if (!line)
+		ft_printf("[ERROR]: File is Empty!");
+	while (line)
+	{
+		map_buffer = ft_strjoin(map_buffer, line);
+		free (line);
+		line = get_next_line(map);
+	}
+	free (line);
+	close(map);
+	game->map = ft_split(map_buffer, '\n');
+	game->mapcopy = ft_split(map_buffer, '\n');
+	free (map_buffer);
 }
 
-int main(int argc, char *argv[])
+void	fill_map(t_game *game, int move_x, int move_y)
 {
-  t_game game;
-
-  if (argc == 2)
-  {
-    read_map(argv[1], &game);
-    ft_printf("%s", game.map);
-  }
-  return (0);
+	if (move_x < 0 || move_y < 0 || move_x >= game->column
+		|| move_y >= game->row || game->mapcopy[move_x][move_y] == '1'
+			|| game->mapcopy[move_x][move_y] == 'X')
+		return ;
+	if (((game->mapcopy[move_x + 1][move_y] == 'E')
+		|| (game->mapcopy[move_x - 1][move_y] == 'E'))
+			&& ((game->mapcopy[move_x][move_y + 1] == '1')
+				|| (game->mapcopy[move_x][move_y - 1] == '1')))
+		return ;
+	if (((game->mapcopy[move_x][move_y + 1] == 'E'
+			|| game->mapcopy[move_x][move_y - 1] == 'E'))
+			&& ((game->mapcopy[move_x + 1][move_y] == '1')
+				|| (game->mapcopy[move_x - 1][move_y] == '1')))
+		return ;
+	if (game->mapcopy[move_x][move_y] == 'E'
+			|| game->mapcopy[move_x][move_y] == 'C')
+		game->mapcopy[move_x][move_y] = '0';
+	game->macopy[move_x][move_y] = 'X';
+	flood_fill(game, move_x - 1, move_y);
+	flood_fill(game, move_x + 1, move_y);
+	flood_fill(game, move_x, move_y - 1);
+	flood_fill(game, move_x, move_y + 1);
 }
