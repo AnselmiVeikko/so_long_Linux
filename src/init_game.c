8e5135b0f53700efd251f6f3c88114884788dc96
Anselmi Henttonen <ahentton@c1r6p1.hive.fi>
2024-09-30 14:23:44 +0300

    changed init_texture function to be more elegant and flexible

--- src/init_game.c
+++ src/init_game.c
@@ -33,22 +33,3 @@ t_game	*init_game(char *argv)
 	game->move_counter = 0;
 	return (game);
 }
-
-void	init_textures(t_game *game)
-{
-	game->temp = mlx_load_png("assets/Exit.png");
-	game->exit = mlx_texture_to_image(game->mlx, game->temp);
-	mlx_delete_texture(game->temp);
-	game->temp = mlx_load_png("assets/Wall.png");
-	game->wall = mlx_texture_to_image(game->mlx, game->temp);
-	mlx_delete_texture(game->temp);
-	game->temp = mlx_load_png("assets/Floor.png");
-	game->floor = mlx_texture_to_image(game->mlx, game->temp);
-	mlx_delete_texture(game->temp);
-	game->temp = mlx_load_png("assets/Player.png");
-	game->player = mlx_texture_to_image(game->mlx, game->temp);
-	mlx_delete_texture(game->temp);
-	game->temp = mlx_load_png("assets/Collectible.png");
-	game->collectible = mlx_texture_to_image(game->mlx, game->temp);
-	mlx_delete_texture(game->temp);
-}
---
+++ src/init_textures.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_textures.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahentton <ahentton@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 13:27:38 by ahentton          #+#    #+#             */
+/*   Updated: 2024/09/30 13:27:49 by ahentton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long.h"
+
+static	mlx_image_t	*load_texture(mlx_t *mlx, const char *path)
+{
+	mlx_texture_t	*temp;
+	mlx_image_t		*prop;
+
+	temp = mlx_load_png(path);
+	prop = mlx_texture_to_image(mlx, temp);
+	mlx_delete_texture(temp);
+	return (prop);
+}
+
+void	init_textures(t_game *game)
+{
+	game->floor = load_texture(game->mlx, "assets/Floor.png");
+	game->wall = load_texture(game->mlx, "assets/Wall.png");
+	game->exit = load_texture(game->mlx, "assets/Exit.png");
+	game->player = load_texture(game->mlx, "assets/Player.png");
+	game->collectible = load_texture(game->mlx, "assets/Collectible.png");
+}
